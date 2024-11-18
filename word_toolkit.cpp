3ae4029baf71a36c4ae357269be90bb9b1f9977b
KaizoIncc <kaizoinc22@gmail.com>
2024-11-18 15:33:25 +0100

    es canonic implementat

--- word_toolkit.cpp
+++ word_toolkit.cpp
@@ -1,1 +1,14 @@
 #include "word_toolkit.hpp"
+
+/*  Pre:  Cert 
+    Post: Retorna cert si i només si les lletres de l'string s estan 
+    en ordre lexicogràfic ascendent. */
+bool word_toolkit::es_canonic(const string& s) throw() {
+    
+    for(int i = 1; i < s.size(); ++i) {
+        if(s[i] < s[i - 1]) return false;
+    }
+
+    return true;
+}
+
