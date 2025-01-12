ab53f10a3be0defadab3ff77d81c99eaad0d20c5
yingxin04 <129186459+yingxin04@users.noreply.github.com>
2025-01-12 20:27:37 +0100

    costos anagrames.cpp

--- anagrames.cpp
+++ anagrames.cpp
@@ -1,23 +1,30 @@
 #include "anagrames.hpp"
 #include "word_toolkit.hpp"
 
+//cost:O(1)
 anagrames::anagrames() throw(error) {
 
 }
 
+//O(n), on n és el nombre d'elements dins de la llista anagram
 anagrames::anagrames(const anagrames& A) throw(error) {
    anagram = A.anagram;
     
 }
-
+//cost:O(1)
 anagrames::~anagrames() throw() {
 
 }
 
+//cost:O(1)
 anagrames& anagrames::operator=(const anagrames& A) throw(error) {
+    if (this != &A) anagram = A.anagram;  
     return *this;
 }
 
+//cost:O(log n + k log k + m + n) m és el nombre d'anagrames canònics ja existents dins de anagram
+//n és la mida de la llista de paraules d'un mateix anagrama
+//k és la longitud de la cadena p
 void anagrames::insereix(const string& p) throw(error) {
     
     diccionari::insereix(p);
@@ -45,7 +52,7 @@ void anagrames::insereix(const string& p) throw(error) {
     }
 }
 
-
+//cost:O(m) on m es el nombre d'anagrames canònics en anagram
 void anagrames::mateix_anagrama_canonic(const string& a, list<string>& L) const throw(error) {
 	if(word_toolkit::es_canonic(a)){
 		
@@ -60,3 +67,4 @@ void anagrames::mateix_anagrama_canonic(const string& a, list<string>& L) const 
 	}
 	else throw error(NoEsCanonic);
 }
+
