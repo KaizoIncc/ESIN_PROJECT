1d3aa301f9dec80e02b9125b960bb38fa9796fb1
yingxin04 <129186459+yingxin04@users.noreply.github.com>
2025-01-12 19:52:20 +0100

    Update obte_paraules.cpp

--- obte_paraules.cpp
+++ obte_paraules.cpp
@@ -6,7 +6,40 @@
     l'string s o k < 3. */
 void obte_paraules::obte_paraules(nat k, const string& s, const anagrames& A, list<string>& paraules) throw(error) {
     
+    if (k < 3 || k > s.size()) throw error(LongitudInvalida);
+
+    iter_subset it(s.size(), k);
+
+    string canonic = word_toolkit::anagrama_canonic(s);
+
+    while (!it.end()) {
+        subset comb = *it;  
+        string combinacio = ""; 
+        
+        for (nat i = 0; i < comb.size(); ++i) {
+            nat idx = comb[i];
+            combinacio += canonic[idx - 1];  
+        }
+
+        list<string> possibles;
+        A.mateix_anagrama_canonic(combinacio, possibles);
+
+        for (list<string>::iterator it_pos = possibles.begin(); it_pos != possibles.end(); ++it_pos) {
+            const string& paraula = *it_pos;
+            auto pos = paraules.begin();
+            while (pos != paraules.end() && *pos < paraula) {
+                ++pos;  
+            }
+
+            if (pos == paraules.end() || *pos != paraula) {
+                paraules.insert(pos, paraula);  
+            }
+        }
+
+        ++it;  
+    }
 }
+
 
 /* Pre:  Cert
     Post: Retorna la llista de paraules que es poden formar usant 3 o més lletres
@@ -14,5 +47,28 @@ void obte_paraules::obte_paraules(nat k, const string& s, const anagrames& A, li
     a igual longitud les paraules estan en ordre alfabètic creixent. 
     Llança un error si l'string s té menys de tres lletres. */
 void obte_paraules::obte_paraules(const string& s, const anagrames& A, list<string>& paraules) throw(error) {
+   
+    if (s.size() < 3) throw error(LongitudInvalida);
 
+    for (nat k = 3; k <= s.size(); ++k) {
+        list<string> temp;
+        obte_paraules(k, s, A, temp); 
+
+        for (list<string>::iterator it = temp.begin(); it != temp.end(); ++it) {
+            const string& paraula = *it;
+
+            auto pos = paraules.begin();
+            while (pos != paraules.end() && (*pos).size() < paraula.size()) {
+                ++pos;  
+            }
+
+            while (pos != paraules.end() && (*pos).size() == paraula.size() && *pos < paraula) {
+                ++pos;
+            }
+
+            if (pos == paraules.end() || *pos != paraula) {
+                paraules.insert(pos, paraula);  
+            }
+        }
+    }
 }
